f7e1ef04957607ef0eb274c3a49c96342c787390
Jaxson Tanner <jaxsonumkc@gmail.com>
2023-10-13 10:03:45 -0500

    Project 9 commit v2

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,33 +1,81 @@
-class Api {
-    constructor(options) {
-      // constructor body
-    }
-  
-    getInitialCards() {
-        getInitialCards() {
-            return fetch("https://around-api.en.tripleten-services.com/v1",, {
-              headers: {
-                authorization: "c56e30dc-2883-4270-a59e-b2f7bae969c6"
-              }
-            })
-              .then(res => {
-                if (res.ok) {
-                  return res.json();
-                }
-
-                return 
-              }); 
-          }
-          
-    }
-  
-    // other methods for working with the API
+export default class Api {
+  constructor({baseUrl, headers}) {
+    this._baseUrl = baseUrl;
+    this._headers = headers;
   }
-  
-  const api = new Api({
-    baseUrl: "https://around-api.en.tripleten-services.com/v1",
-    headers: {
-      authorization: "c56e30dc-2883-4270-a59e-b2f7bae969c6",
-      "Content-Type": "application/json"
+
+  _checkResponse(res) {
+    if (res.ok) {
+      return res.json();
     }
-  });
+    return Promise.reject(`Error: ${res.status}`);
+  }
+
+  getUserInfo() {
+    return fetch(`${this._baseUrl}/users/me`, {
+      headers: {
+        authorization: this._headers
+      }
+    })
+    .then(res => this._checkResponse(res));
+  }
+
+  getInitialCards() {
+    return fetch(`${this._baseUrl}/cards`, {
+      headers: {
+        authorization: this._authToken
+      }
+    })
+    .then(res => this._checkResponse(res));
+  }
+
+  createCard({ name, link }) {
+    return fetch(`${this._baseUrl}/cards`, {
+      method: "POST",
+      body: JSON.stringify({ name, link }),
+      headers: this._headers,
+    }).then(this._checkResponse);
+  }
+
+
+  updateEditProfile({ name, about }) {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: "PATCH",
+      body: JSON.stringify({ name, about }),
+      headers: this._headers,
+      }).then(this._checkResponse);
+    }
+
+  deleteCard(cardId) {
+    return fetch(`${this._baseUrl}/cards/${cardId}`, {
+      method: "DELETE",
+      headers: this._headers,
+      }).then(this._checkResponse);
+    }
+
+  likeCard(cardId) {
+    return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+      method: "PUT",
+      headers: this._headers,
+      }).then(this._checkResponse);
+    }
+
+  unlikeCard(cardId) {
+    return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+      method: "DELETE",
+      headers: this._headers,
+      }).then(this._checkResponse);
+    }
+    
+  updateAvatar(updatedAvatar) {
+    return fetch(`${this._baseUrl}/users/me/avatar`, {
+      method: "PATCH",
+      body: JSON.stringify({ avatar: updatedAvatar }),
+      headers: this._headers,
+      }).then(this._checkResponse);
+    }
+
+  getAllData() {
+    return Promise.all([this._getUserInfo(), this._getInitialCards()]);
+  }
+}
